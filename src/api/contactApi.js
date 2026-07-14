2327c1b48a3c015505b093c651f2de615d1fede9
mohanasrijyesta-ops <mohanasri.jyesta@gmail.com>
2026-07-14 21:07:24 +0530

    Update contactApi.js

--- src/api/contactApi.js
+++ src/api/contactApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://contactmanager1-csmb-mohana.onrender.com"
+    baseURL: "https://contactmanagerf-csmb-mohana.onrender.com"
 });
 
 export default API;
